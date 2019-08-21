e7e37e3b32f957eb2fde14b28c483f293ef8de3c
John Chen <johnchen456@gmail.com>
2019-08-21 12:15:33 -0400

    test: fixed issues with lint

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -1,31 +1,33 @@
 # frozen_string_literal: true
+# rubocop:disable LineLength
 
-describe command ("/opt/java/jdk5/bin/java -version 2>&1 | sed -n '1p' | awk '{print $3}'") do
+describe command("/opt/java/jdk5/bin/java -version 2>&1 | sed -n '1p' | awk '{print $3}'") do
   its('exit_status') { should eq 0 }
-  its('stdout') { should eq "\"1.5.0_22\"\n"}
+  its('stdout') { should eq "\"1.5.0_22\"\n" }
 end
 
-describe command ("/opt/java/jdk6/bin/java -version 2>&1 | sed -n '1p' | awk '{print $3}'") do
+describe command("/opt/java/jdk6/bin/java -version 2>&1 | sed -n '1p' | awk '{print $3}'") do
   its('exit_status') { should eq 0 }
-  its('stdout') { should eq "\"1.6.0_45\"\n"}
+  its('stdout') { should eq "\"1.6.0_45\"\n" }
 end
 
-describe command ("/opt/java/jdk7/bin/java -version 2>&1 | sed -n '1p' | awk '{print $3}'") do
+describe command("/opt/java/jdk7/bin/java -version 2>&1 | sed -n '1p' | awk '{print $3}'") do
   its('exit_status') { should eq 0 }
-  its('stdout') { should eq "\"1.7.0_79\"\n"}
+  its('stdout') { should eq "\"1.7.0_79\"\n" }
 end
 
-describe command ("/opt/java/jdk8/bin/java -version 2>&1 | sed -n '1p' | awk '{print $3}'") do
+describe command("/opt/java/jdk8/bin/java -version 2>&1 | sed -n '1p' | awk '{print $3}'") do
   its('exit_status') { should eq 0 }
-  its('stdout') { should eq "\"1.8.0_162\"\n"}
+  its('stdout') { should eq "\"1.8.0_162\"\n" }
 end
 
-describe command ("/opt/java/jdk9/bin/java -version 2>&1 | sed -n '1p' | awk '{print $3}'") do
+describe command("/opt/java/jdk9/bin/java -version 2>&1 | sed -n '1p' | awk '{print $3}'") do
   its('exit_status') { should eq 0 }
-  its('stdout') { should eq "\"9\"\n"}
+  its('stdout') { should eq "\"9\"\n" }
 end
 
-describe command ("/opt/java/jdk11/bin/java -version 2>&1 | sed -n '1p' | awk '{print $3}'") do
+describe command("/opt/java/jdk11/bin/java -version 2>&1 | sed -n '1p' | awk '{print $3}'") do
   its('exit_status') { should eq 0 }
-  its('stdout') { should eq "\"11.0.1\"\n"}
+  its('stdout') { should eq "\"11.0.1\"\n" }
 end
+# rubocop:enable LineLength
